3d46e781bb945d319a707a794d662c1b4ead4d0e
leamind24 <melindatmmt@gmail.com>
2025-05-12 09:25:26 +0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,38 +1,10 @@
-const FORECASTS = [
+const compliments = [
   'Пусть сладкое на кухне никогда не кончается',
   'Пусть снитч летит на желтую мантию, а не наоборот',
   'Для тебя у меня на диване в общей гостиной всегда есть свободное местечко',
-]
+];
 
-let shownIndices = [];
-
-const getRandomIndex = () => Math.floor(Math.random() * FORECASTS.length);
-
-const handleClick = () => {
-  const forecastNode = document.querySelector('.forecast');
-  
-  if (!forecastNode) {
-    return;
-  }
-  
-  if (shownIndices.length === FORECASTS.length) {
-    shownIndices = [shownIndices[shownIndices.length - 1]];
-  }
-  
-  let randomIndex = getRandomIndex();
-  
-  while(shownIndices.includes(randomIndex)) {
-    randomIndex = getRandomIndex();
-  }
-
-  const randomForecast = FORECASTS[randomIndex];
-  shownIndices.push(randomIndex);
-  
-  forecastNode.textContent = randomForecast;
-}
-
-const button = document.querySelector('.button');
-
-if (button) {
-  button.addEventListener('click', handleClick)
+function generateCompliment() {
+  const compliment = compliments[Math.floor(Math.random() * compliments.length)];
+  document.getElementById("compliment").textContent = compliment;
 }
